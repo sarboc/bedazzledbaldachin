9967052d4e13a638e115311792041bf937cbdaa0
Sara LaFassett <sara@lafassett.com>
2013-11-08 17:04:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bedazzle::Application.config.secret_token = '57bb3fdd0e263fb5e046d7ac5926416172b6261b50cd73ceb7f5ae6c7d805f982459c4a2d62a574ce2660e156a304412786a3d2cff84038766c266eef48238b2'
