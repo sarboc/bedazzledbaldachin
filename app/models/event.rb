6f2ab53e225951e863e7d76aae22984927804913
Francisco ALberini <f.alberini@gmail.com>
2013-11-12 09:39:59 -0800

    More model updates including has_many, belongs_to associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,15 @@
 class Event < ActiveRecord::Base
-  attr_accessible :end_time, :event_status, :rating_id, :start_time, :wordnik
+  attr_accessible :end_time, :event_status, :rating_id, :start_time, :wordnik, :party_type_id
 
+  has_many :event_users, dependent: :destroy
   has_many :users, through: :event_users
+  has_many :event_prompts, dependent: :destroy
   has_many :prompts, through: :event_prompts
   belongs_to :event_user
   belongs_to :rating
-  has_many :event_prompts
-  has_one :party_type
+  belongs_to :party_type
 
   validates :rating_id, presence: true
   validates :wordnik, presence: true
-
+  validates :party_type_id, presence: true
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,9 +1,10 @@
 class Prompt < ActiveRecord::Base
   attr_accessible :category_type, :description, :rating_id, :requirements
 
+  has_many :party_type_prompts, dependent: :destroy
   has_many party_types:, through: :party_type_prompts
   belongs_to :event_prompts
-  has_one :rating
+  belongs_to :rating
 
   validates :description, presence: true
   validates :category_type, presence: true
