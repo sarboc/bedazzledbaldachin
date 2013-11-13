813a1510c409b86892e491dc2080999eb1ae66a3
mkyriacou <strattica@gmail.com>
2013-11-13 09:13:30 -0800

    db changes for event owner

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,11 +8,19 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
-    @user = User.new
+ 
+    # MK commenting this out - user is the current_user and we know that
+    #@user = User.new
   end
 
   def create
 
+    # MK adding these two lines.  Is this 'create' hash correct? 
+    @event = Event.create(:rating_id => params[:rading_id], :party_type_id => params[:party_type_id])
+    # note owner is added in the after_create scope in the model
+    @owner = current_user
+    #check syntax below
+    render :json => @event + @owner
   end
 
   def show
